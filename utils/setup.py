46bc70f317b8d2e92b8b9f630cf01fbff4120885
pppppass <lzh2016p@pku.edu.cn>
2018-11-14 16:50:28 +0800

    Rename utilities

--- utils/setup.py
+++ utils/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 import numpy
 
-exts = Extension("exts", sources=["exts/algos.c", "exts/wrappers.c"], include_dirs=[numpy.get_include()], libraries=["mkl_rt"])
+exts = Extension("exts", sources=["exts/cg.c", "exts/wrappers.c"], include_dirs=[numpy.get_include()], libraries=["mkl_rt"])
 
 setup(ext_modules=[exts])
